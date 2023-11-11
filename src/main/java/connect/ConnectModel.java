de81fa7953ded84abce893fa9f8051e3fad80172
Collins C. A <contactmrc4@gmail.com>
2023-11-11 12:22:44 +0100

    added connect class with methods to create and display table

--- src/main/java/connect/ConnectModel.java
+++ src/main/java/connect/ConnectModel.java
@@ -1,4 +1,28 @@
 package connect;
 
+import java.util.Arrays;
+
 public class ConnectModel {
+
+    public int[][] createBoard(int row, int colomn) {
+        return new int[colomn][row];
+    }
+
+    public void printTable(int[][] table) {
+        for (int[] ints : table) {
+            for (int anInt : ints) {
+                System.out.print("      [ "+anInt+" ]");
+            }
+            System.out.println("\n");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ConnectModel connectModel = new ConnectModel();
+
+        connectModel.printTable(connectModel.createBoard(7, 6));
+    }
+
+
 }
