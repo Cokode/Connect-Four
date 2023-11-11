376e5d60161dfc5c76f24311030f70e21bc77358
Collins C. A <contactmrc4@gmail.com>
2023-11-11 13:42:18 +0100

    created addToBoard() and display() method

--- src/main/java/connect/ConnectModel.java
+++ src/main/java/connect/ConnectModel.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class ConnectModel {
 
+    // can be integrated as class property
     public int[][] createBoard(int row, int colomn) {
         return new int[colomn][row];
     }
@@ -11,10 +12,27 @@ public class ConnectModel {
     public void printTable(int[][] table) {
         for (int[] ints : table) {
             for (int anInt : ints) {
-                System.out.print("      [ "+anInt+" ]");
+                System.out.print("   [ "+anInt+" ]");
             }
             System.out.println("\n");
         }
+    }
+
+    public String displayBoard() {
+        return
+        """
+        
+        """;
+    }
+    private boolean addToBoard(int position, int player, int[][] table) {
+        for (int i = table.length-1; i > 0; --i) {
+            if ((table[i][position]) < 0){
+                table[i][position] = player;
+                return true;
+            }
+        }
+
+        return false;
     }
 
 
